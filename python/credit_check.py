afc64f2aa51fa7d6bbd063cd8f89c44e18109855
Evan Garcia <evanag116@yahoo.com>
2022-05-26 09:25:37 -0500

    completed in py

--- python/credit_check.py
+++ python/credit_check.py
@@ -1,11 +1,10 @@
 def credit_check(str):
-    # num = [int(i) for i in reversed(str)]
-    # for i in range(0, len(list(reversed(str))), 2):
-    #     print(list(reversed(str))[i])
-    # return 0
+    num = [int(i) for i in str]
+    doubles_of_every_other = [(( ((num[i]*2) // 10) + ((num[i]*2) % 10) ) if (num[i]*2 > 9) else num[i]*2) if i%2 == 0 else num[i] for i in range(len(num))]
+    
+    # return doubles_of_every_other
+    return "The number is valid!" if sum(doubles_of_every_other) % 10 == 0 else "The number is invalid!"
 
-    x = sum([i*2 for i in range(0, len(list(reversed(str))), 2)])
-    return x
 
 # Your Luhn Algorithm Here
 # Expected Output:

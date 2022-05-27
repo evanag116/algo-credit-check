98aa0057ec9ef535ab34c9cb95a8203c71f68466
Evan Garcia <evanag116@yahoo.com>
2022-05-26 21:14:02 -0500

    completed in js

--- js/creditCheck.js
+++ js/creditCheck.js
@@ -1,2 +1,18 @@
 exports.creditCheck = function(str) {
+    str = str.split("")
+    let sum = 0
+    for (let i = 0; i < str.length; i++) {
+        if(i%2 == 0) {
+            str[i] *= 2
+            if (str[i] > 9) {
+                str[i] = (str[i] % 10) + parseInt(str[i]/10)
+            }
+        } else {
+            str[i] *= 1
+        }
+        sum += str[i]
+    }
+    return (sum % 10 == 0) ? "The number is valid!" : "The number is invalid!"
 }
+
+
